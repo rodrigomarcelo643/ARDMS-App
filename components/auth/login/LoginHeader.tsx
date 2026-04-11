1fce4e7d6da3e1469a7433acc73356bc47131477
Marcelo <rodrigomarcelo643@gmail.com>
2026-04-11 22:43:01 +0800

    feat: add component LoginHeader UI

---
+++ components/auth/login/LoginHeader.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, Image, Animated } from 'react-native';
+import RotatingDots from "@/components/ui/RotatingDots";
+
+interface LoginHeaderProps {
+  logoStyle: any;
+  textStyle: any;
+}
+
+export const LoginHeader: React.FC<LoginHeaderProps> = ({
+  logoStyle,
+  textStyle,
+}) => (
+  <View className="items-center mb-8">
+    <Animated.View style={logoStyle}>
+      <Image
+        source={require("../../../assets/images/swu-head.png")}
+        className="w-20 h-20 mb-1"
+        accessibilityLabel="App logo"
+      />
+    </Animated.View>
+
+    <Animated.View style={textStyle}>
+      <Text className="text-5xl whitespace-nowrap text-center font-extrabold tracking-wide">
+        <Text
+          style={{
+            color: "#af1616",
+            fontWeight: "900",
+            textShadowColor: "rgba(0,0,0,0.3)",
+            textShadowOffset: { width: 1, height: 2 },
+            textShadowRadius: 3,
+          }}
+        >
+          Med
+        </Text>
+        
+        <Text
+          style={{
+            color: "#15803d",
+            fontWeight: "900",
+            textShadowColor: "rgba(0,0,0,0.3)",
+            textShadowOffset: { width: 1, height: 2 },
+            textShadowRadius: 3,
+          }}
+        >
+          SIS
+        </Text>
+      </Text>
+    </Animated.View>
+
+    <RotatingDots />
+  </View>
+);
